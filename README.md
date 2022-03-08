2e4bdcc5a691dab0a548d1da2d51c77210100359
ashawin <ashwanimishra@Ashwanis-MacBook-Air.local>
2022-03-08 11:20:52 +0530

    change for music app demo latest

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# nodepro_test
+# musician-app
+NodeJS / React sample app for AWS CI/CD pipeline tutorial
+
+https://www.youtube.com/watch?v=NwzJCSPSPZs
--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const path = require('path');
+const store = require('./store/datastore');
+const initialStoreData = require('./store/data');
+const Musician = require('./models/musician');
+const musicianRoutes = require('./routes/musician');
+
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello World! Ashwani');
+// include routes
+app.use('/musician', musicianRoutes);
+
+app.use(express.static('public'));
+
+// Index route
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at  Anna illa http://localhost:${port}`);
+// initialize store
+const musician = new Musician(store);
+musician.initStore(initialStoreData);
+app.locals.musician = musician;
+
+// start server
+const server = app.listen(port, () => {
+  console.log("Server started on port " + port);
 });
+
+module.exports = server;
